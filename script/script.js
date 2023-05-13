0b30d5aa662a55c7881319ace2a194a025e70bf5
alfinxd4 <alfinxd4@gmail.com>
2023-05-14 02:22:24 +0700

    vers-skilvul

--- script/script.js
+++ script/script.js
@@ -5,11 +5,70 @@ hamburger.addEventListener("click", () => {
   navLink.classList.toggle("hide");
 });
 
-function show_hide() {
+const show_hide = () => {
   const click = document.querySelector(".dropdown");
   if (click.style.display === "none") {
     click.style.display = "block";
   } else {
     click.style.display = "none";
+  }
+};
+
+function handleGetFormData() {
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let city = document.getElementById("city").value;
+  let zipCode = document.getElementById("zip-code").value;
+  let status = document.getElementById("status").checked;
+
+  return {
+    name,
+    email,
+    city,
+    zipCode,
+    status,
+  };
+}
+
+function isNumber(string) {
+  if (isNaN(string) == false) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function checkboxIsChecked() {
+  let status = document.getElementById("status").checked;
+
+  if (status == true) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function validateFormData(data) {
+  if (!data) {
+    return false;
+  }
+  if (!isNumber(data.zipCode)) {
+    return false;
+  }
+  if (!checkboxIsChecked()) {
+    return false;
+  }
+  return true;
+}
+
+function submit() {
+  if (validateFormData() == false) {
+    document
+      .getElementById("warning")
+      .innerHTML("Periksa form anda sekali lagi");
+  } else {
+    document
+    .getElementById("warning")
+    .innerHTML("");
   }
 }
